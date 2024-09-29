6d446f7f7510653af02872ad5f453ebfc6343d06
eduardopr14 <eduardo.ruli@solinftec.com.br>
2024-09-29 16:56:50 -0300

    feat: adicionando busca

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { SearchIcon } from "lucide-react"
-import { Button } from "./_components/ui/button"
-import { Input } from "./_components/ui/input"
+import { SearchComponent } from "../app/posts/search/SearchComponent"
 import PostItem from "./_components/post-item"
 
 const Home = async () => {
   let data: Response
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let posts: any
+  let posts: Posts.Post[] = []
+
   try {
     data = await fetch("http://localhost:3108/posts")
     posts = await data.json()
@@ -17,16 +15,9 @@ const Home = async () => {
   return (
     <div className="w-full">
       <div className="p-5 lg:px-24">
-        <div className="mb-4 flex items-center gap-6">
-          <Input placeholder="Faça sua busca" />
-
-          <Button>
-            <SearchIcon />
-          </Button>
-        </div>
-
+        <SearchComponent />
         <div className="space-y-4 overflow-y-auto">
-          {posts && posts.length > 0 &&
+          {posts.length > 0 &&
             posts.map((post: Posts.Post) => (
               <PostItem key={post.id} post={post} />
             ))}
